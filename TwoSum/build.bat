195d2708a430f67dc1d3cf1ef15d0fd7562b8128
feedfood <codingboy@gmail.com>
2018-01-24 16:34:46 +0800

    update 001 sum two number

--- TwoSum/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64     
-c:
-cd %USERPROFILE%/source/repos/leetcode/TwoSum
-set compilerflags=/Od /Zi /EHsc
-set linkerflags=/OUT:TwoSum.exe
-cl.exe %compilerflags% TwoSum.cpp /link %linkerflags%
-
-TwoSum.exe
