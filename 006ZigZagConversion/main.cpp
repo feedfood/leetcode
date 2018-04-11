99395e9f9fed98c9cb9083a65daddf5b9306c9c2
feedfood <codingboy@gmail.com>
2018-04-11 18:14:58 +0800

    06 zigzag finished

--- 006ZigZagConversion/main.cpp
+++ 006ZigZagConversion/main.cpp
@@ -4,13 +4,30 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <exception>
+#include <sstream>
 
 using namespace std;
 
 class Solution {
 public:
     string convert(string s, int numRows) {
-        return "";
+        if( s.size() <= numRows || numRows <= 1) {
+            return s;
+        }
+        
+        stringstream ret;
+        int x_step = 2*numRows-2;
+        for(int i=0;i<numRows; i++) {
+            for(int n=0; i+n<s.size(); n+=x_step) {
+                ret << s[i+n];
+
+                if( i>0 && i<numRows-1 && i+n+x_step-2*i <s.size()) {
+                    ret << s[i+n+x_step-2*i];
+                }
+            }
+        }
+
+        return ret.str();
     }
 };
 
@@ -26,6 +43,61 @@ int main(){
         std::cout << " OK ";
     } else {
         std::cout << " ERROR ";
+    }
+    std::cout << endl;
+
+    //case 2: abc -> abc
+    std::cout << "case 2: abc -> abc ";
+    input = "abc";
+    answer = s.convert(input,3);
+    if( answer == "abc" ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer.c_str();
+    }
+    std::cout << endl;
+
+    //case 3: abcd -> adbc
+    std::cout << "case 3: abcd -> abdc ";
+    input = "abcd";
+    answer = s.convert(input,3);
+    if( answer == "abdc" ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer.c_str();
+    }
+    std::cout << endl;
+
+    //case 4: abcde -> aebdc
+    std::cout << "case 4: abcde -> aebdc ";
+    input = "abcde";
+    answer = s.convert(input,3);
+    if( answer == "aebdc" ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer.c_str();
+    }
+    std::cout << endl;
+
+    //case 5: abcde -> aebdc
+    std::cout << "case 5: abcde -> abced ";
+    input = "abcde";
+    answer = s.convert(input,4);
+    if( answer == "abced" ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer.c_str();
+    }
+    std::cout << endl;
+
+    //case 5: ab -> ab
+    std::cout << "case 5: ab -> ab ";
+    input = "ab";
+    answer = s.convert(input,1);
+    if( answer == "ab" ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer.c_str();
     }
     std::cout << endl;
 }
