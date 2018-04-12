54a85aa98a804e4f5465afd0b7f576ea50e77869
feedfood <codingboy@gmail.com>
2018-04-12 15:39:18 +0800

    007 1 commit

--- 007ReverseInteger/main.cpp
+++ 007ReverseInteger/main.cpp
@@ -11,7 +11,26 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        return x;
+        if( x < 10 ) {
+            return x;
+        }
+        int r = x % 10;
+        x = x / 10;
+        int ret = r;
+
+        r = x % 10;
+        x = x / 10;
+        ret = ret * 10 + r;
+
+        if( x == 0 ) {
+            return ret;
+        }
+        
+        r = x % 10;
+        x = x / 10;
+        ret = ret * 10 + r;
+
+        return ret;
     }
 };
 
@@ -30,5 +49,25 @@ int main(){
     }
     std::cout << endl;
 
+    //case 2: 12->21
+    std::cout << "case 2: 12->21 ";
+    input = 12;
+    answer = s.reverse(input);
+    if( answer==  21 ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer;
+    }
+    std::cout << endl;
     
+    //case 3: 123->321
+    std::cout << "case 3: 123->321 ";
+    input = 123;
+    answer = s.reverse(input);
+    if( answer==  321 ) {
+        std::cout << " OK ";
+    } else {
+        std::cout << " ERROR, actual is " << answer;
+    }
+    std::cout << endl;
 }
